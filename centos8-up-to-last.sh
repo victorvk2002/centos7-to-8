a0326ba20bb3fcbbaa0d66af5a96cfff24cc05bb
Victor <victorvk2002@mail.ru>
2025-09-20 19:46:08 +0300

    Update centos8-up-to-last.sh

--- centos8-up-to-last.sh
+++ centos8-up-to-last.sh
@@ -4,11 +4,11 @@ source msg.sh
 
 if [ ! -f "STAGE3_DONE.flag" ]
 then
-  msg "удалить старые ядра"
+  msg "centos8: удалить старые ядра"
   dnf remove -y --oldinstallonly --setopt installonly_limit=1
   rpm -qa | grep kernel
 
-  msg "подтягиваем версии пакетов к ядру"
+  msg "centos8: подтягиваем версии пакетов к ядру"
   dnf -y --releasever=8 --allowerasing --setopt=deltarpm=false distro-sync
   # будут ошибки зависимостей
   rpm -Va --nofiles --nodigest
@@ -20,5 +20,6 @@ then
   dnf remove -y sysvinit-tools-2.88-14.dsf.el7.x86_64 python36-rpmconf-1.1.7-1.el7.1.noarch
   dnf -y --releasever=8 --allowerasing --setopt=deltarpm=false distro-sync
 
+  msg "centos8: очистка завершена"
   touch STAGE3_DONE.flag
 fi
