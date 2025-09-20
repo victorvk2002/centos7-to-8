a6bb389f5072382e565870f4b5cd96f862d89b34
Victor <victorvk2002@mail.ru>
2025-09-20 20:51:46 +0300

    Update centos8-up-to-last.sh

--- centos8-up-to-last.sh
+++ centos8-up-to-last.sh
@@ -20,6 +20,11 @@ then
   dnf remove -y sysvinit-tools-2.88-14.dsf.el7.x86_64 python36-rpmconf-1.1.7-1.el7.1.noarch
   dnf -y --releasever=8 --allowerasing --setopt=deltarpm=false distro-sync
 
+  dnf install -y NetworkManager
+  systemctl enable NetworkManager
+  systemctl start NetworkManager
+  systemctl status NetworkManager
+
   msg "centos8: очистка завершена"
   touch STAGE3_DONE.flag
 fi
