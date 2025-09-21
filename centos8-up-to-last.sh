a29dd3af9effb390f2a4a264f073e4f6201d5afb
Victor <victorvk2002@mail.ru>
2025-09-21 17:12:11 +0300

    Update centos8-up-to-last.sh

--- centos8-up-to-last.sh
+++ centos8-up-to-last.sh
@@ -6,29 +6,8 @@ source msg.sh
 
 if [ ! -f "STAGE3_DONE.flag" ]
 then
-  msg "centos8: удалить старые ядра"
-  dnf remove -y --oldinstallonly --setopt installonly_limit=1
-  rpm -qa | grep --color kernel
-
-  msg "контрольный"
-  dnf -y --releasever=8 --allowerasing --setopt=deltarpm=false distro-sync
-
   msg "подчищаем ненужное"
   dnf -y autoremove
-  # # будут ошибки зависимостей
-  # rpm -Va --nofiles --nodigest
-  # rpm --rebuilddb
-  # dnf clean all
-  # dnf makecache
-  # rpm -e dracut-network --nodeps
-  # dnf -y --releasever=8 --allowerasing --setopt=deltarpm=false distro-sync
-  # dnf remove -y sysvinit-tools-2.88-14.dsf.el7.x86_64 python36-rpmconf-1.1.7-1.el7.1.noarch
-  # dnf -y --releasever=8 --allowerasing --setopt=deltarpm=false distro-sync
-
-  # dnf install -y NetworkManager
-  # systemctl enable NetworkManager
-  # systemctl start NetworkManager
-  # systemctl status NetworkManager
 
   msg s "centos8: очистка завершена. неплохо будет перезагрузиться и убедиться что всё точно ок."
   touch STAGE3_DONE.flag
