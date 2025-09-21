4270edf6857818345a2c3d5c9d74db905e1c7018
Victor <victorvk2002@mail.ru>
2025-09-21 14:17:19 +0300

    Update centos7-to-8.sh

--- centos7-to-8.sh
+++ centos7-to-8.sh
@@ -13,9 +13,35 @@ then
   
   dnf install -y centos-linux-release-8.5-1.2111.el8.noarch.rpm centos-gpg-keys-8-3.el8.noarch.rpm centos-linux-repos-8-3.el8.noarch.rpm
 
+  rm -rf /etc/yum.repos.d/CentOS*
+  rm -rf /etc/yum.repos.d/epel*
+
+#   # Repo
+  cat > /etc/yum.repos.d/centos-vault.repo << 'EOF'
+[BaseOS]
+name=CentOS-8 - Base
+baseurl=http://vault.centos.org/centos/8/BaseOS/x86_64/os/
+enabled=1
+gpgcheck=0
+
+[AppStream]
+name=CentOS-8 - AppStream
+baseurl=http://vault.centos.org/centos/8/AppStream/x86_64/os/
+enabled=1
+gpgcheck=0
+
+[extras]
+name=CentOS-8 - Extras
+baseurl=http://vault.centos.org/centos/8/extras/x86_64/os/
+enabled=1
+gpgcheck=0
+EOF
+
+  dnf repolist
   dnf clean all
   dnf makecache
   dnf update -y
+  dnf -y --releasever=8 --allowerasing --setopt=deltarpm=false distro-sync
   
   msg "centos7: установка ядра centos8"
   wget http://vault.centos.org/8.5.2111/BaseOS/x86_64/os/Packages/linux-firmware-20210702-103.gitd79c2677.el8.noarch.rpm
@@ -23,8 +49,14 @@ then
   wget http://vault.centos.org/8.5.2111/BaseOS/x86_64/os/Packages/kernel-4.18.0-348.7.1.el8_5.x86_64.rpm
   wget http://vault.centos.org/8.5.2111/BaseOS/x86_64/os/Packages/kernel-modules-4.18.0-348.7.1.el8_5.x86_64.rpm
   wget http://vault.centos.org/8.5.2111/BaseOS/x86_64/os/Packages/kernel-tools-libs-4.18.0-348.7.1.el8_5.x86_64.rpm
+
+  msg w "директоря /lib/modules/ до установки ядра"
+  ls -lhF /lib/modules/
   
   dnf install -y --allowerasing linux-firmware-20210702-103.gitd79c2677.el8.noarch.rpm kernel-4.18.0-348.7.1.el8_5.x86_64.rpm kernel-core-4.18.0-348.7.1.el8_5.x86_64.rpm kernel-modules-4.18.0-348.7.1.el8_5.x86_64.rpm kernel-tools-libs-4.18.0-348.7.1.el8_5.x86_64.rpm
+
+  msg w "директоря /lib/modules/ после установки ядра"
+  ls -lhF /lib/modules/
 
   msg "centos7: фикс ошибки сборки ядра centos8"
   # появится ошибка Symvers dump file /boot/symvers-4.18.0-348.7.1.el8_5.x86_64.gz not found
