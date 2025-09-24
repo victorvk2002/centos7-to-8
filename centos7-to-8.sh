f48b7732ad53326bc715af2d2ec295e3b48b342d
Victor <victorvk2002@mail.ru>
2025-09-24 13:52:04 +0300

    Update centos7-to-8.sh

--- centos7-to-8.sh
+++ centos7-to-8.sh
@@ -6,36 +6,36 @@ source msg.sh
 
 if [ ! -f "STAGE2_DONE.flag" ]
 then
-  msg "Подключаем репозитории CentOS 8 + EPEL"
-  wget https://vault.centos.org/8.5.2111/BaseOS/x86_64/os/Packages/centos-linux-release-8.5-1.2111.el8.noarch.rpm
-  wget https://vault.centos.org/8.5.2111/BaseOS/x86_64/os/Packages/centos-gpg-keys-8-3.el8.noarch.rpm
-  wget https://vault.centos.org/8.5.2111/BaseOS/x86_64/os/Packages/centos-linux-repos-8-3.el8.noarch.rpm
-
-  dnf install -y \
-      centos-linux-release-8.5-1.2111.el8.noarch.rpm \
-      centos-gpg-keys-8-3.el8.noarch.rpm \
-      centos-linux-repos-8-3.el8.noarch.rpm
-
+  msg "подключаем репозитории centos8 + epal"
+  wget https://vault.centos.org/centos/8/BaseOS/x86_64/os/Packages/centos-linux-release-8.5-1.2111.el8.noarch.rpm
+  wget https://vault.centos.org/centos/8/BaseOS/x86_64/os/Packages/centos-gpg-keys-8-3.el8.noarch.rpm
+  wget https://vault.centos.org/centos/8/BaseOS/x86_64/os/Packages/centos-linux-repos-8-3.el8.noarch.rpm
+  
+  dnf install -y centos-linux-release-8.5-1.2111.el8.noarch.rpm centos-gpg-keys-8-3.el8.noarch.rpm centos-linux-repos-8-3.el8.noarch.rpm
   dnf upgrade -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 
-  msg "Правим репозитории на vault.centos.org"
-  sed -i 's|^mirrorlist=|#mirrorlist=|g' /etc/yum.repos.d/*.repo
-  sed -i 's|^#baseurl=http://mirror.centos.org|baseurl=https://vault.centos.org|g' /etc/yum.repos.d/*.repo
-  sed -i 's|^baseurl=http://mirror.centos.org|baseurl=https://vault.centos.org|g' /etc/yum.repos.d/*.repo
-  grep --color -vE "^#|^$" /etc/yum.repos.d/*.repo
-  
-  msg "Чистим пакеты CentOS7"
-  dnf -y remove sysvinit-tools || true
-  dnf -y remove dracut-network || true
-  dnf -y remove python36-rpmconf || true
+  msg "снова поправляем съехавшие репозитарии"
+  grep --color -r "mirror" /etc/yum.repos.d/
+  sed -i s/mirror.centos.org/vault.centos.org/g /etc/yum.repos.d/*.repo
+  sed -i s/^#.*baseurl=http/baseurl=https/g /etc/yum.repos.d/*.repo
+  sed -i s/^mirrorlist=http/#mirrorlist=https/g /etc/yum.repos.d/*.repo
+  sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+  sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 
-  msg "Distro-sync"
+  msg "чистим мусор"
+  rpm -e --nodeps sysvinit-tools
+  rpm -e kernel-3.10.0-1160.80.1.el7 kernel-3.10.0-1160.119.1.el7
+  
+  dnf -y remove dracut-network
+  dnf -y remove python36-rpmconf-1.1.7-1.el7.1.noarch
+
+  msg "distro-sync"
   dnf -y --releasever=8 --allowerasing --setopt=deltarpm=false distro-sync
 
-  msg "Ставим ядро и минимальные группы"
+  msg "ставим ядро"
   dnf -y install kernel-core
   dnf -y groupupdate "Core" "Minimal Install"
-
-  msg "Обновление завершено. Требуется перезагрузка."
+  
+  msg s "сборка ядра centos8 завершена. требуется перезагрузка"
   touch STAGE2_DONE.flag
 fi
